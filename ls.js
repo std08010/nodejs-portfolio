3e57b071b4e87705500763ed9bd0dbdf28eda937
acipi <haris.tsipis@gmail.com>
2022-08-20 13:19:55 +0300

    script to list files in folder

---
+++ ls.js
@@ -0,0 +1,15 @@
+//const fs = require('fs').promises;
+import { promises as fs } from "fs";
+
+async function listFiles() {
+  try {
+    const files = await fs.readdir(".");
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+listFiles();
