1a34120248e8f950839c6e6ed811f1b58e7e520b
acipi <haris.tsipis@gmail.com>
2022-08-20 16:08:35 +0300

    use direct function call and arrow function

--- ls.js
+++ ls.js
@@ -1,22 +1,19 @@
 const fs = require("fs").promises; //require the Promises version of the fs library so that it can be used with the await keyword.
 // import { promises as fs } from "fs";
 
-async function listFiles() {
-  try {
-    var dir = ".";
+(async () => {
+  var dir = ".";
 
-    //Check if user provided a directory argument
-    if (process.argv[2]) {
-      dir = process.argv[2];
-    }
-
-    const files = await fs.readdir(dir);
-    for (const file of files) {
-      console.log(file);
-    }
-  } catch (err) {
-    console.error(err);
+  //Check if user provided a directory argument
+  if (process.argv[2]) {
+    dir = process.argv[2];
   }
-}
 
-listFiles();
+  const files = await fs.readdir(dir);
+  for (const file of files) {
+    console.log(file);
+  }
+})().catch((err) => {
+  //it is very important to catch any error of inline functions
+  console.error(err);
+});
