e72031ca92f9a6b4fbdb29bd586181587b17b31c
acipi <haris.tsipis@gmail.com>
2022-08-20 19:23:44 +0300

    added a simple module implementation

---
+++ simple-module.js
@@ -0,0 +1,16 @@
+//this variable is private to the module because is not attached to module.exports variable
+var count = 0;
+
+module.exports.next = function () {
+  return ++count;
+};
+
+exports.hello = () => {
+  return "Hello World!\n";
+};
+
+//both above declarations of attaching a function to the public interface of the module are equivalent
+
+//You can use this module like this:
+//const simple = require('./simple-module');
+//simple.hello();
