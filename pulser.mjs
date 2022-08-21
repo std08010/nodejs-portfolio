8e9aa4e1b8ad721dd142714b76c6a21ee095c6d2
acipi <haris.tsipis@gmail.com>
2022-08-21 20:51:10 +0300

    simple eventemitter class

---
+++ pulser.mjs
@@ -0,0 +1,11 @@
+import EventEmitter from "events";
+
+export class Pulser extends EventEmitter {
+  start() {
+    setInterval(() => {
+      console.log(`${new Date().toISOString()} >>>> pulse`);
+      this.emit("pulse");
+      console.log(`${new Date().toISOString()} <<<< pulse`);
+    }, 1000);
+  }
+}
